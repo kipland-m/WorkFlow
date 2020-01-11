e838c45222087c420f9736d632739bf679af4a71
Kipland Melton <kipmelton@yahoo.com>
2020-01-11 03:53:07 -0600

    Add files via upload

---
+++ WorkFlow.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+
+def Main():
+
+    webbrowser.open('https://trello.com/kipmelton/boards',new=1,autoraise=True)
+    webbrowser.open('https://github.com/kipland-m',new=2,autoraise=True)
+    webbrowser.open('https://mail.google.com/mail/u/0/?hl=en#inbox',new=2,autoraise=True)
+    webbrowser.open('https://stackoverflow.com/',new=2,autoraise=True)
+
+Main()
